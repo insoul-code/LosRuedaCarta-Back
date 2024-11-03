87a43e60835cb9d333808bf21e9c1100e855b26e
unknown <tiago931220@gmail.com>
2024-11-03 11:24:36 -0500

    Se realiza borrado del bloque creado previamente ya que no se va a utilizar

--- comun/comun-aplicacion-comando/src/main/java/com/losruedacarta/core/aplicacion/manejador/ManejadorComando.java
+++
@@ -1,9 +0,0 @@
-package com.losruedacarta.core.aplicacion.manejador;
-
-import org.springframework.transaction.annotation.Transactional;
-
-public interface ManejadorComando<C> {
-
-	@Transactional
-	void ejecutar(C comando);
-}
--- comun/comun-aplicacion-comando/src/main/java/com/losruedacarta/core/aplicacion/manejador/ManejadorComandoRespuesta.java
+++
@@ -1,9 +0,0 @@
-package com.losruedacarta.core.aplicacion.manejador;
-
-import org.springframework.transaction.annotation.Transactional;
-
-public interface ManejadorComandoRespuesta<C, R> {
-
-	@Transactional
-	R ejecutar(C comando);
-}
